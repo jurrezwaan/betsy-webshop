232897e05bbedcef35a869da7c1a9d2b5f9733c2
jurrezwaan <jurrezwaan@gmail.com>
2022-10-18 13:46:37 +0200

    classes maken

--- models.py
+++ models.py
@@ -1,1 +1,29 @@
 # Models go here
+import peewee
+
+db = peewee.SqliteDatabase("database.db")
+
+
+class BaseModel(peewee.Model):
+    class Meta:
+        database = db
+
+
+class Product(BaseModel):
+    name = peewee.CharField()
+    description = peewee.TextField()
+    price = peewee.FloatField()
+    quantity = peewee.IntegerField()
+
+
+class User(BaseModel):
+    name = peewee.CharField()
+    address = peewee.TextField()
+    billing_info = peewee.TextField()
+    products_owned = peewee.ManyToManyField(Product)
+
+
+class Transaction():
+    user_id = peewee.ForeignKeyField(User)
+    product_id = peewee.ForeignKeyField(Product)
+    quantity = peewee.IntegerField()
