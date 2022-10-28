a17f2f955ea87fcfc2a50d0b5472227740c164c3
jurrezwaan <jurrezwaan@gmail.com>
2022-10-28 13:20:30 +0200

    purchase_product lukt niet

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ __human_name__ = "Betsy Webshop"
 from models import *
 
 
+def main():
+    # print(search('glockenspiel'))
+    # list_user_products(1)
+    # print(list_products_per_tag(4))
+    # # add_product_to_catalog(1, 'shampoo bar')
+    # update_stock(1, 4)
+    purchase_product(1, 2, 1)
+
+
 def search(term):
-    return Product.select().where(Product.name.contains(term))
+    return [product for product in Product.select().where(Product.name.contains(term))]
 
 
 def list_user_products(user_id):
@@ -17,23 +26,35 @@ def list_user_products(user_id):
 
 
 def list_products_per_tag(tag_id):
-    return [product.name for product in Product.select().join(ProductTag).join(Tag).where(Tag.id == tag_id)]
+    return [tag.product.name for tag in Tag.select(Tag, Product).join(Product).where(Tag.id == tag_id)]
 
 
 def add_product_to_catalog(user_id, product):
     user = User.get(User.id == user_id)
-    user.products_owned.add(Product.select().where(Product.name == product))
+    user.products_owned.add(Product.get(Product.name == product))
 
 
 def update_stock(product_id, new_quantity):
-    ...
+    product_name = Product.get(product_id == product_id).name
+    (Product
+     .update(quantity=new_quantity)
+     .where(Product.id == product_id)
+     .execute())
+    print('{}''s new stock : {}'.format(product_name, new_quantity))
 
 
 def purchase_product(product_id, buyer_id, quantity):
-    ...
+    product = Product.get(Product.id == product_id)
+    buyer = User.get(User.id == buyer_id)
+    Transaction.create(user_id == buyer, product_id ==
+                       product, quantity == quantity)
 
 
 def remove_product(user_id, product_id):
     user = User.get(User.id == user_id)
     user.products_owned.remove(
         Product.select().where(Product.id == product_id))
+
+
+if __name__ == "__main__":
+    main()
--- models.py
+++ models.py
@@ -17,7 +17,7 @@ class Product(BaseModel):
 
 
 class Tag(BaseModel):
-    name = peewee.CharField()
+    name = peewee.CharField(unique=True)
     product = peewee.ForeignKeyField(Product, backref='tags')
 
 
@@ -28,9 +28,9 @@ class User(BaseModel):
     products_owned = peewee.ManyToManyField(Product, backref='users')
 
 
-class Transaction():
-    user_id = peewee.ForeignKeyField(User, backref='transactions')
-    product_id = peewee.ForeignKeyField(Product, backref='transactions')
+class Transaction(BaseModel):
+    user_id = peewee.ForeignKeyField(User)
+    product_id = peewee.ForeignKeyField(Product)
     quantity = peewee.IntegerField()
 
 
@@ -50,13 +50,19 @@ def populate_test_data():
          25, 1, ('jewellery', 'silver', 'french')),
         ('clogs', '''Clogs are a type of footwear made in part or completely from wood.
         Used worldwide, their forms can vary by culture,
-        but often remained unchanged for centuries within a culture.''', 45, 1, ('shoes', 'wood', 'dutch'))
+        but often remained unchanged for centuries within a culture.''', 45, 1, ('shoes', 'wood', 'dutch')),
+        ('shampoo bar', '''Enjoy a natural, intense purification of your hair and skin. For a truly pure and squeaky clean feeling.''',
+         7.99, 1, ('shampoo', 'soap', 'vegan')),
+        ('trucker cap', '''What is especially striking about the HFT RipStop Trucker Cap from Djinns is 
+         the airy mesh insert at the back that provides excellent ventilation. 
+         The snapback cap has press studs on the open back for an extra good fit. 
+         The curved peak measures 7 cm and the crown 10.5 cm.''', 21.95, 1, ('cap', 'brown', 'trucker'))
     ]
 
     user_data = [
-        ('claus', 'clausstrasse 1', 'DB10203040', 'glockenspiel')
-        ('emma', 'rue de emma 1', 'FB20103050', 'necklace')
-        ('bert', 'bertplantsoen 1' 'NB30501020', 'clogs')
+        ('claus', 'clausstrasse 1', 'DB10203040', ['glockenspiel']),
+        ('emma', 'rue de emma 1', 'FB20103050', ['necklace']),
+        ('bert', 'bertplantsoen 1', 'NB30501020', ['clogs'])
     ]
 
     for name, description, price, quantity, tags in product_data:
@@ -66,8 +72,7 @@ def populate_test_data():
             Tag.create(name=tag, product=product)
 
     for name, address, billing_info, products_owned in user_data:
-
-
-populate_test_data()
-
-# bedenk eerst goed welke tables nodig zijn. quantity bij product class en geen quantity van aangekochte artikelen bij houden
+        user = User.create(
+            name=name, address=address, billing_info=billing_info)
+        for product in products_owned:
+            user.products_owned.add(Product.get(Product.name == product))
